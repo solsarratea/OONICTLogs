8a1a93a60cbc6239cac4825e295914dad00e25bb
solsarratea <solquemal@gmail.com>
2024-05-15 17:23:04 +0200

    abstract queries into a new package

---
+++ finder/ooni/query_single_measurement.go
@@ -0,0 +1,23 @@
+package ooni
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func QuerySingleMeasurement(string apiEndpoint) ([]byte, error) {
+	response, err := http.Get(apiEndpoint)
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error: API returned status code %d", response.StatusCode)
+	}
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %v", err)
+	}
+
+	return body, nil
+}
