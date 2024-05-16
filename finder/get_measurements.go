2a516cf9a103f6d123f2fc2211f265e8bec32cac
solsarratea <solquemal@gmail.com>
2024-05-16 20:16:10 +0200

    isolate getting raw measurements

---
+++ finder/get_measurements.go
@@ -0,0 +1,52 @@
+package finder
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"./measurements"
+	"./utils"
+)
+
+func QueryMeasurements(config Configuration) ([]byte, error) {
+	//since: 2023-12-20T00%3A00%3A00
+	//until: 2024-07-20T00%3A00%3A00
+
+	apiEndpoint := fmt.Sprintf("https://api.ooni.io/api/v1/measurements?test_name=web_connectivity&since=%s&until=%s&failure=false&order_by=measurement_start_time&order=asc&limit=100", config.OONIMeasurements.Since, config.OONIMeasurements.Until)
+
+	fmt.Printf(apiEndpoint)
+
+	response, err := http.Get(apiEndpoint)
+	if err != nil {
+		return nil, fmt.Errorf("error fetching data from API: %v", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error: API returned status code %d", response.StatusCode)
+	}
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %v", err)
+	}
+	return body, nil
+}
+
+func GetRawMeasurements(config Configuration) {
+	body, err := QueryMeasurements(config)
+	if err != nil {
+		fmt.Printf("Failed to query measurements: %v\n", err)
+
+	} else {
+
+		rawMeasurements, err := measurements.DecodeMeasurements(body)
+
+		if err != nil {
+			fmt.Printf("Failed to decode raw measurements %v\n", err)
+		} else {
+			utils.WriteMeasurementsToFile(rawMeasurements)
+		}
+	}
+}
