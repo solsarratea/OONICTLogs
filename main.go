284ed2525dd913661ecc8f6fbf740b36cef48b17
solsarratea <solquemal@gmail.com>
2024-09-10 18:31:16 +0200

    add external module for handle flags

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 
 	"github.com/solsarratea/OONICTLogs/finder"
 	"github.com/solsarratea/OONICTLogs/submitter"
@@ -10,30 +10,16 @@ import (
 
 // Receives OS arguments and starts: finder and submitter processes
 func main() {
+	find := flag.Bool("find", false, "Enable the finder process")
+	submit := flag.Bool("submit", false, "Enable the submit process")
+	flag.Parse()
+
 	fmt.Println("Starting OONICTLogs...")
-
-	ENABLE_FINDER := false
-	ENABLE_SUBMITTER := false
-
-	if len(os.Args) == 1 || os.Args[1] == "all" {
-		ENABLE_FINDER = true
-		ENABLE_SUBMITTER = true
-	} else {
-		for _, arg := range os.Args[1:] {
-			switch arg {
-			case "find":
-				ENABLE_FINDER = true
-			case "submit":
-				ENABLE_SUBMITTER = true
-			}
-		}
-	}
-
-	if ENABLE_FINDER {
+	if *find {
 		go finder.Start()
 	}
 
-	if ENABLE_SUBMITTER {
+	if *submit {
 		go submitter.Start()
 	}
 
